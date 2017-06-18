5b0bd5ca5bbb3c54423fe7fa71f9b612c7eeb93d
keatshyperion <hyperions1996@gmail.com>
2017-06-18 11:23:06 +0200

    do

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-
 var done = false;
 
 
 
-    
-$(window).scroll(function() {
 
-  if ($(window).scrollTop() > 80 && $(window).scrollTop() < 599) {
-         $(".nawigacja").fadeOut(500);
-     console.log("hi");
-    } 
-    else if ($(window).scrollTop() < 80 ) {
-    
-        $(".nawigacja").fadeIn(500);
-       $(".nawigacja").css('position', 'static')
-        
-   } 
-   //ss
-    else if ($(window).scrollTop() > 600 ) {
-         
-   // }
-       $(".nawigacja").fadeIn(500);
-       $(".nawigacja").css('position','fixed');
-       $(".nawigacja").css('left','0px');
+$(window).scroll(function () {
+
+
+    if ($(window).scrollTop() < 200) {
+        $(".nawigacja").finish();
+        $(".nawigacja").fadeIn(0);
+        $(".nawigacja").css('position', 'relative')
+
+    } else if ($(window).scrollTop() >= 200 && $(window).scrollTop() <= 599) {
+
+        $(".nawigacja").fadeOut(300);
+
+    }
+    //ss
+    else if ($(window).scrollTop() >= 610) {
+
+        // }
+
+
+
+        $(".nawigacja").fadeIn(300);
+        $(".nawigacja").css('position', 'fixed');
        
-   }
 
- });
+    }
+
+});
 
 //lel
 //lel
 var get_navbar_collapse = document.getElementsByClassName('navbar-collapse');
 
-window.addEventListener('resize',function(){
-get_navbar_collapse[0].classList.remove("show");
+window.addEventListener('resize', function () {
+    get_navbar_collapse[0].classList.remove("show");
 
 
 
@@ -42,15 +45,15 @@ get_navbar_collapse[0].classList.remove("show");
 
 getHamburger = document.getElementsByClassName('hamburger');
 
-getHamburger[0].addEventListener('click',function(){
-this.classList.toggle('is-active');
+getHamburger[0].addEventListener('click', function () {
+    this.classList.toggle('is-active');
 
 })
 
-window.addEventListener('resize',function(){
+window.addEventListener('resize', function () {
 
-if(window.innerWidth > 980){
-     getHamburger[0].classList.remove('is-active');
-}
+    if (window.innerWidth > 980) {
+        getHamburger[0].classList.remove('is-active');
+    }
 
 })
