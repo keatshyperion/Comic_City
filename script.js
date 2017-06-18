0455cad9f09ce611d879fc204ba0bde522b6a9dc
keatshyperion <hyperions1996@gmail.com>
2017-06-18 11:27:15 +0200

    patch 21.0

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(window).scroll(function () {
 
         $(".nawigacja").fadeIn(300);
         $(".nawigacja").css('position', 'fixed');
-       
+       $(".nawigacja").css('position', 'fixed');
 
     }
 
